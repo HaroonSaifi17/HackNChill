91d001589dcc43b09a8fd20d92bdf5e3a2c35478
Kunal89204 <Kunalkhandelwal108@gmail.com>
2024-02-09 19:25:16 +0530

    created sponsor component

--- src/component/sponser/Sponser.jsx
+++ src/component/sponser/Sponser.jsx
@@ -1,10 +1,142 @@
-import './Sponser.css'
-import React from 'react'
+import React from "react";
 
 function Sponser() {
   return (
-    <div>Sponser</div>
-  )
+    <div className="bg-black pb-10 sm:pb-20">
+
+      <div className="flex text-3xl sm:text-5xl justify-center py-6 sm:py-10">
+        <div className="bg-gradient-to-r from-gray-500 to-white text-transparent bg-clip-text">
+          MEET OUR
+        </div>
+        <div className="bg-gradient-to-r from-red-600 to-red-900 ml-2 sm:ml-4 px-1">
+          SPONSORS
+        </div>
+      </div>
+
+      <div className="flex justify-center px-4 py-10 sm:px-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 sm:gap-14 ">
+
+          <div
+            className="h-64 w-64 lg:w-44 lg:h-44 bg-white cursor-pointer"
+            style={{ transition: "box-shadow 0.35s" }}
+            onMouseEnter={(e) =>
+              (e.currentTarget.style.boxShadow = "-6px 6px 0px red")
+            }
+            onMouseLeave={(e) => (e.currentTarget.style.boxShadow = "none")}
+          >
+            <img
+              src=""
+              alt="sponsor_image"
+              className="h-full w-full object-cover"
+            />
+          </div>
+          <div
+            className="h-64 w-64 lg:w-44 lg:h-44 bg-white cursor-pointer"
+            style={{ transition: "box-shadow 0.35s" }}
+            onMouseEnter={(e) =>
+              (e.currentTarget.style.boxShadow = "-6px 6px 0px red")
+            }
+            onMouseLeave={(e) => (e.currentTarget.style.boxShadow = "none")}
+          >
+            <img
+              src=""
+              alt="sponsor_image"
+              className="h-full w-full object-cover"
+            />
+          </div>
+          <div
+            className="h-64 w-64 lg:w-44 lg:h-44 bg-white cursor-pointer"
+            style={{ transition: "box-shadow 0.35s" }}
+            onMouseEnter={(e) =>
+              (e.currentTarget.style.boxShadow = "-6px 6px 0px red")
+            }
+            onMouseLeave={(e) => (e.currentTarget.style.boxShadow = "none")}
+          >
+            <img
+              src=""
+              alt="sponsor_image"
+              className="h-full w-full object-cover"
+            />
+          </div>
+          <div
+            className="h-64 w-64 lg:w-44 lg:h-44 bg-white cursor-pointer"
+            style={{ transition: "box-shadow 0.35s" }}
+            onMouseEnter={(e) =>
+              (e.currentTarget.style.boxShadow = "-6px 6px 0px red")
+            }
+            onMouseLeave={(e) => (e.currentTarget.style.boxShadow = "none")}
+          >
+            <img
+              src=""
+              alt="sponsor_image"
+              className="h-full w-full object-cover"
+            />
+          </div>
+          <div
+            className="h-64 w-64 lg:w-44 lg:h-44 bg-white cursor-pointer"
+            style={{ transition: "box-shadow 0.35s" }}
+            onMouseEnter={(e) =>
+              (e.currentTarget.style.boxShadow = "-6px 6px 0px red")
+            }
+            onMouseLeave={(e) => (e.currentTarget.style.boxShadow = "none")}
+          >
+            <img
+              src=""
+              alt="sponsor_image"
+              className="h-full w-full object-cover"
+            />
+          </div>
+          <div
+            className="h-64 w-64 lg:w-44 lg:h-44 bg-white cursor-pointer"
+            style={{ transition: "box-shadow 0.35s" }}
+            onMouseEnter={(e) =>
+              (e.currentTarget.style.boxShadow = "-6px 6px 0px red")
+            }
+            onMouseLeave={(e) => (e.currentTarget.style.boxShadow = "none")}
+          >
+            <img
+              src=""
+              alt="sponsor_image"
+              className="h-full w-full object-cover"
+            />
+          </div>
+          <div
+            className="h-64 w-64 lg:w-44 lg:h-44 bg-white cursor-pointer"
+            style={{ transition: "box-shadow 0.35s" }}
+            onMouseEnter={(e) =>
+              (e.currentTarget.style.boxShadow = "-6px 6px 0px red")
+            }
+            onMouseLeave={(e) => (e.currentTarget.style.boxShadow = "none")}
+          >
+            <img
+              src=""
+              alt="sponsor_image"
+              className="h-full w-full object-cover"
+            />
+          </div>
+          <div
+            className="h-64 w-64 lg:w-44 lg:h-44 bg-white cursor-pointer"
+            style={{ transition: "box-shadow 0.35s" }}
+            onMouseEnter={(e) =>
+              (e.currentTarget.style.boxShadow = "-6px 6px 0px red")
+            }
+            onMouseLeave={(e) => (e.currentTarget.style.boxShadow = "none")}
+          >
+            <img
+              src=""
+              alt="sponsor_image"
+              className="h-full w-full object-cover"
+            />
+          </div>
+
+
+        
+
+          
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Sponser
+export default Sponser;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-     "./index.html",
-    "./src/*.tsx",
-    "./src/**/*.tsx",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
