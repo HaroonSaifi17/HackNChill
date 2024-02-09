9b04aa3a8c1cf05d17b1c0f7349c2e212e271ebe
ArchitJ6 <architjain1417@gmail.com>
2024-02-09 22:28:52 +0530

    Footer Done!!!

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,10 +1,55 @@
-import './Footer.css'
-import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Footer.css";
+import React from "react";
+import {
+  faFacebook,
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faMedium,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
-function Footer() {
+const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="">
+        <h1 className="text-red-700 text-2xl md:text-3xl font-bold border-b-2 border-b-white pb-5 md:pr-5">Let's get you registered!</h1>
+      </div>
+      <div className="footer-menu">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Timeline</a>
+        <a href="#">FAQs</a>
+      </div>
+      <div className="footer-menu">
+        <a href="#">Sponsors</a>
+        <a href="#">Prizes</a>
+        <a href="#">Events</a>
+      </div>
+      <div className="footer-menu md:items-start md:gap-4">
+        <h2 className="font-semibold text-lg">EXPLORE US!</h2>
+        <p>hello.hacknchill@gmail.com</p>
+        <div className="footer-icon">
+          <a href="#">
+            <FontAwesomeIcon size="xl" icon={faInstagram} />
+          </a>
+          <a href="#">
+            <FontAwesomeIcon size="xl" icon={faGithub} />
+          </a>
+          <a href="#">
+            <FontAwesomeIcon size="xl" icon={faLinkedin} />
+          </a>
+          <a href="#">
+            <FontAwesomeIcon size="xl" icon={faXTwitter} />
+          </a>
+          <a href="#">
+            <FontAwesomeIcon size="xl" icon={faMedium} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
