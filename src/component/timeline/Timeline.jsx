a852178a8e9a521ff1df20b2e9160bcb5014bd1e
LakshayGMZ <lakshaygulati1898@gmail.com>
2024-02-21 14:55:54 +0530

    quick changes

--- src/component/timeline/Timeline.jsx
+++ src/component/timeline/Timeline.jsx
@@ -18,33 +18,15 @@ function Timeline() {
 
   return (
     <div id="timeline" className="timeline-wpr">
-      <div>
+      <div className={"flex flex-col"}>
         <h2 className="text-white text-4xl text-center w-full py-5 px-[5vw]">
           TIMELINE
         </h2>
         <div className="h-[10vh]"></div>
         <div
           style={{ transform: `translateX(${scrollPosition}px)` }}
-          className="train"
-        >
-          <div className="absolute w-60 bg-white p-2 rounded-2xl rounded-tl-none rounded-br-none top-0 right-[1120px] text-center">
-            <p className=" font-semibold text-xl ">Registration Start</p>
-            <p className="font-normal text-md">10 March 2023</p>
-            <span className="absolute bg-white w-[2px] h-[58px] top-full right-0">
-              <span className="absolute bg-white w-[2px] h-[120px] top-[180px] right-0"></span>
-            </span>
-          </div>
-          <div className="absolute w-60 bg-white p-2 rounded-2xl rounded-tl-none rounded-br-none top-0 right-[1427px] font-semibold text-xl text-center">
-            Registration End
-            <br /> <span className="font-normal text-md">30 March 2023</span>
-            <span className="absolute bg-white w-[2px] h-[295px] top-full right-0"></span>
-          </div>
-          <div className="absolute w-60 bg-white p-2 rounded-2xl rounded-tl-none rounded-br-none top-0 left-[26px] font-semibold text-xl text-center">
-            Event Start
-            <br /> <span className="font-normal text-md">31 March 2023</span>
-            <span className="absolute bg-white w-[2px] h-[295px] top-full right-0"></span>
-          </div>
-        </div>
+          className="train flex-grow-1"
+        ></div>
         <div className="track"></div>
       </div>
     </div>
