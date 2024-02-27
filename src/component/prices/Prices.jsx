c605b652a6c530361c975b1abca67771be293d3a
Mohd Haroon <haroonsaifi2001@gmail.com>
2024-02-27 19:32:47 +0530

    prices updated

--- src/component/prices/Prices.jsx
+++ src/component/prices/Prices.jsx
@@ -5,23 +5,22 @@ function Prices() {
   return (
     <div id="prizes" className="prices-bg">
       <div className="max-w-screen-2xl mx-auto px-[5vw] py-16">
-        <h3 className="text-center font-semibold text-white  font-semibold text-5xl font-ubuntu">
+        <h3 className="text-center font-semibold text-white text-5xl font-ubuntu">
           PRIZES
         </h3>
         <div className="flex justify-around items-center max-md:flex-col gap-7 mt-12 max-md:mb-0  mb-[200px]">
-          <div className="flex-col flex items-center gap-5 max-md:order-1 order11  order-2">
-            <div className="inline-block mx-auto  w-52 h-52 border-white border-[12px] rounded-md"></div>
-            <h1 className="text-white text-4xl font-semibold">#1</h1>
-            <p className="text-white text-center ">
-              Our premium product with top-notch performance and durability.
-              Ideal for demanding tasks and long-term use.
+          <div className="flex-col flex items-center gap-4 max-md:order-1 order11  order-2">
+            <div className="inline-block mx-auto  w-48 h-48 border-white border-[12px] rounded-md"></div>
+            <h1 className="text-white text-4xl text-center font-semibold mt-2">Winner: $300</h1>
+            <p className="text-white text-center  px-2">
+              Winners will be awarded with cash prize and Many more surprises for the winners
             </p>
           </div>
-          <div className="mb-[-200px] max-md:mb-0 justify-start flex-col flex gap-5 items-center max-md:order-2  order-1 ">
+          <div className="mb-[-200px] max-md:mb-0 justify-start flex-col flex gap-4 items-center max-md:order-2  order-1 ">
             <svg
               viewBox="0 0 100 100"
               xmlns="http://www.w3.org/2000/svg"
-              className="w-52 mx-auto"
+              className="w-48 mx-auto"
             >
               <polygon
                 points="50,5 95,95 5,95"
@@ -30,18 +29,16 @@ function Prices() {
                 strokeWidth="6"
               />
             </svg>
-            <h1 className="text-white text-4xl font-semibold">#2</h1>
-            <p className="text-white text-center">
-              This is a high-quality product with advanced features. It's
-              perfect for professionals and enthusiasts alike.
+            <h1 className="text-white text-4xl font-semibold text-center mt-2">2nd Place: $200</h1>
+            <p className="text-white text-center px-2">
+              Runner Up team will be awarded with cash prize along with gifts too.
             </p>
           </div>
-          <div className="mb-[-200px] flex-col flex items-center gap-5 max-md:mb-0 order-3">
-            <div className=" w-52 h-52 border-[#DB0000] border-[12px] inline-block rounded-full"></div>
-            <h1 className="text-white text-4xl font-semibold">#3</h1>
-            <p className="text-white text-center">
-              A budget-friendly option without compromising on quality. Great
-              for beginners and casual users.
+          <div className="mb-[-200px] flex-col flex items-center gap-4 max-md:mb-0 order-3">
+            <div className=" w-48 h-48 border-[#DB0000] border-[12px] inline-block rounded-full"></div>
+            <h1 className="text-white text-center text-4xl font-semibold mt-2">3rd Place: $150</h1>
+            <p className="text-white text-center px-2">
+              Team acquires Third place will be awarded with cash prize along with surprise gifts
             </p>
           </div>
         </div>
